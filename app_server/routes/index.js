319ace17de9b5f97514b904f49784d2d2c126efc
chaitanyakrishnabojja <39226470+chaitanyakrishnabojja@users.noreply.github.com>
2018-12-08 14:12:58 -0500

    added views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const ctrlLocations = require('../controllers/locations'); 
+const ctrlOthers = require('../controllers/others');
 
-const ctrlMain = require('../controllers/main'); 
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo); 
+router.get('/location/review/new', ctrlLocations.addReview); 
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
